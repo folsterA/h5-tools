f20d8f065269fbe03caa7ab9752100ffd750e9ff
folsterA <folsteraustin@gmail.com>
2025-10-17 22:58:17 -0700

    Put bindings in ffi module

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,19 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+pub mod ffi {
+    #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
 
 #[cfg(test)]
 mod tests {
+    use super::ffi;
     use std::ffi::CString;
-
-    use super::*;
 
     #[test]
     fn round_trip_h5() {
         let file_name = CString::new("../test.h5").unwrap();
         unsafe {
-            let err = H5Fopen(file_name.as_ptr(), 0, H5P_DEFAULT.into());
+            ffi::H5Eset_auto2(ffi::H5E_DEFAULT.into(), None, std::ptr::null_mut());
+            let err = ffi::H5Fopen(file_name.as_ptr(), 0, ffi::H5P_DEFAULT.into());
             assert!(err == 0);
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use h5_tools::ffi;
+
 fn main() {
-    println!("Hello, world!");
+    let double_type: ffi::hid_t;
+    unsafe {
+        // all of this is to encode a IEEE 754 type without pre-processor macros from HDF5...
+        double_type = ffi::H5Tcreate(ffi::H5T_class_t_H5T_FLOAT, 8);
+        ffi::H5Tset_ebias(double_type, 1023);
+        ffi::H5Tset_norm(double_type, ffi::H5T_norm_t_H5T_NORM_IMPLIED);
+        ffi::H5Tset_order(double_type, ffi::H5T_order_t_H5T_ORDER_LE);
+        ffi::H5Tset_pad(
+            double_type,
+            ffi::H5T_pad_t_H5T_PAD_ZERO,
+            ffi::H5T_pad_t_H5T_PAD_ZERO,
+        );
+        ffi::H5Tset_precision(double_type, 64);
+        ffi::H5Tset_fields(double_type, 63, 52, 11, 0, 52);
+    }
+    println!("{double_type}");
 }
